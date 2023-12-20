04772c65195f56b399a88b3ada7dd3631b214329
Venkata Siddardha Rali <sidhurv8@gmail.com>
2023-12-20 16:25:20 +0530

    template for dashboard and item page are set

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html') 
+    product = Product.objects.all()
+    return render(request,'index.html',{'product':product}) 
+
+def product(request,pk):
+    product = Product.objects.get(id=pk)
+    products = Product.objects.all()
+    context = {'product':product,'products':products}
+    return render(request,'item.html',context)
