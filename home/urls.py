d9d9d0d1f9ee64248116a66c0fc8e261eca59972
Venkata Siddardha Rali <sidhurv8@gmail.com>
2024-01-02 22:14:10 +0530

    rendered items

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('product/<int:pk>',views.product,name='product'),
+    path('cart/',views.cart,name='cart'),
+    path('checkout',views.checkout,name='checkout'),
+    path('thanks',views.thanks,name='thanks'),
+    path('register',views.register,name='register'),
+    path('forgotpass',views.forgotpass,name='forgotpass'),
 ]
--- home/views.py
+++ home/views.py
@@ -8,6 +8,28 @@ def index(request):
 
 def product(request,pk):
     product = Product.objects.get(id=pk)
-    products = Product.objects.all()
+    products = Product.objects.all()[4:]
     context = {'product':product,'products':products}
     return render(request,'item.html',context)
+
+def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer=customer,complete=False)
+        item = order.orderitem_set.all()
+        context = {'items':item}
+    else:
+        item = []
+    return render(request,'cart.html',context)
+
+def checkout(request):
+    return render(request,'checkout.html')
+
+def thanks(request):
+    return render(request,'thanks.html')
+
+def register(request):
+     return render(request,'login.html')
+
+def forgotpass(request):
+    return render(request,'forgot_pass.html')
